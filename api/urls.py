6951d5fbe69b67f53c77ae73a500c4226cd4df91
wayne chan <waynestock666@gmail.com>
2024-06-02 04:08:45 +0100

    update userauths

--- api/urls.py
+++ api/urls.py
@@ -5,10 +5,12 @@ from store import views as store_views
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path('user/token/', userauths_views.MyTokenObtainPairView.as_view()),
-    path('user/token/refresh/', TokenRefreshView.as_view()),
-    path('user/register/', userauths_views.RegisterView.as_view()),
-    path('user/password-reset/<email>', userauths_views.PasswordResetEmailVerify.as_view()),
+    path('user/token/', userauths_views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('user/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/register/', userauths_views.RegisterView.as_view(), name='auth_register'),
+    path('user/password-reset/<email>', userauths_views.PasswordResetEmailVerify.as_view(), name='user_profile'),
+    path('user/password-change/', userauths_views.PasswordChangeView.as_view(), name='password_change'),
+
 
 
 ]
